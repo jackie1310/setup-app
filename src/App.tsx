ba07f450468dc12635fa232e2c1a40eb91f29244
NGUYEN AN <nguyenan521462@hotmail.com>
2023-12-28 07:28:44 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col gap-4 items-center'>
+      <h1 className='text-4xl text-red-500'>Hello Code with Mentor</h1>
+      <p className='text-xl'>This is just a setup</p>
     </div>
   );
 }
